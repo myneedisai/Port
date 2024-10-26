ddb607490345f715483cf0e4220f0ae29cb00cfe
myneedisai <myneedisai@gmail.com>
2024-10-26 15:10:29 -0500

    Update variables.tf

--- S3_Create/variables.tf
+++ S3_Create/variables.tf
@@ -6,11 +6,5 @@ variable "s3_bucket_name" {
 variable "s3_acl" {
   description = "The canned ACL to apply to the bucket"
   type        = string
-  default     = "private" # You can adjust this as needed
-}
-
-variable "aws_region" {
-  description = "The AWS region to create the S3 bucket in"
-  type        = string
-  default     = "us-east-1" # Change to your desired region
+  default     = "private"
 }
