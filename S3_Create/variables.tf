060afd94bf5574b14327ad0ed0714c894159bee7
myneedisai <myneedisai@gmail.com>
2024-10-26 15:53:27 -0500

    Update variables.tf

--- S3_Create/variables.tf
+++ S3_Create/variables.tf
@@ -2,6 +2,13 @@ variable "s3_bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
+
 variable "aws_region" {
-  type = string
+  description = "The AWS region"
+  type        = string
+}
+
+variable "s3_acl" {
+  description = "The canned ACL to apply to the bucket"
+  type        = string
 }
