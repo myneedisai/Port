1841e687ae6feafd6e707847515ea0c71f1aa0e1
myneedisai <myneedisai@gmail.com>
2024-10-27 09:28:40 -0500

    Update variables.tf

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -1,7 +1,5 @@
 variable "aws_region" {
-  description = "The AWS region to deploy the resources in"
   type        = string
-  default     = "us-west-1"  # Change as necessary
 }
 
 variable "ec2_name" {
@@ -9,18 +7,13 @@ variable "ec2_name" {
 }
 
 variable "ami_id" {
-  description = "The AMI ID to use for the EC2 instance"
   type        = string
-  default     = "ami-0da424eb883458071"
 }
 
 variable "ec2_instance_type" {
-  description = "The type of EC2 instance"
   type        = string
-  default     = "t2.micro"  # Change as necessary
 }
 
 variable "pem_key_name" {
-  description = "The key name to use for the EC2 instance"
   type        = string
 }
