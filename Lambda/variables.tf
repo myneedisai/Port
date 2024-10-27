8a7d8f7d9897e6cd1371df516b99ddd236b82739
myneedisai <myneedisai@gmail.com>
2024-10-27 10:47:18 -0500

    Create variables.tf

---
+++ Lambda/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "lambda_name" {
+  type = string
+}
+
+variable "lambda_runtime" {
+  type = string
+}
+
+variable "lambda_handler" {
+  type = string
+}
+
+variable "s3_bucket" {
+  type = string
+}
+
+variable "s3_key" {
+  type = string
+}
