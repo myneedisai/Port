c42aa2b80b9bf07412c62b7bea5e787a96384f67
myneedisai <myneedisai@gmail.com>
2024-10-26 20:48:42 -0500

    Update main.tf

--- S3_Create/main.tf
+++ S3_Create/main.tf
@@ -21,19 +21,3 @@ resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
 }
 
 
-# Optionally define a bucket policy if you need specific access controls
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
-      },
-    ]
-  })
-}
-
