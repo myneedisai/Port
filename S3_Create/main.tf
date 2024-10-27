c10865e1e1313ecf627f2388a9d7986b190865f2
myneedisai <myneedisai@gmail.com>
2024-10-26 20:16:51 -0500

    Update main.tf

--- S3_Create/main.tf
+++ S3_Create/main.tf
@@ -9,11 +9,15 @@ resource "aws_s3_bucket" "my_bucket" {
     Name        = var.s3_bucket_name
     Environment = "Dev" # Adjust as needed
   }
+}
 
-  block_public_acls  = true
-  ignore_public_acls  = true
-  block_public_policy = true
-  restrict_public_buckets = true
+resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets  = true
 }
 
 
