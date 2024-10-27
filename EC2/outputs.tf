66968ffd3b1835cc3f471a886d296db46a23d553
myneedisai <myneedisai@gmail.com>
2024-10-27 09:33:24 -0500

    Update outputs.tf

--- EC2/outputs.tf
+++ EC2/outputs.tf
@@ -1,63 +1,63 @@
 output "instance_id" {
-  value       = aws_instance.var.ec2_name.id
+  value       = aws_instance.nginx_instance.id
 }
 
 output "instance_state" {
   description = "The state of the EC2 instance (e.g., running, stopped)."
-  value       = aws_instance.var.ec2_name.instance_state
+  value       = aws_instance.nginx_instance.instance_state
 }
 
 output "instance_type" {
   description = "The type of EC2 instance (e.g., t2.micro, m5.large)."
-  value       = aws_instance.var.ec2_name.instance_type
+  value       = aws_instance.nginx_instance.instance_type
 }
 
 output "availability_zone" {
   description = "The Availability Zone where the EC2 instance is deployed."
-  value       = aws_instance.var.ec2_name.availability_zone
+  value       = aws_instance.nginx_instance.availability_zone
 }
 
 output "public_dns" {
   description = "The public DNS name assigned to the EC2 instance."
-  value       = aws_instance.var.ec2_name.public_dns
+  value       = aws_instance.nginx_instance.public_dns
 }
 
 output "public_ip" {
   description = "The public IP address assigned to the EC2 instance."
-  value       = aws_instance.var.ec2_name.public_ip
+  value       = aws_instance.nginx_instance.public_ip
 }
 
 output "private_dns" {
   description = "The private DNS name assigned to the EC2 instance within its VPC."
-  value       = aws_instance.var.ec2_name.private_dns
+  value       = aws_instance.nginx_instance.private_dns
 }
 
 output "private_ip" {
   description = "The private IP address assigned to the EC2 instance within its VPC."
-  value       = aws_instance.var.ec2_name.private_ip
+  value       = aws_instance.nginx_instance.private_ip
 }
 
 output "monitoring" {
   description = "Indicates if detailed monitoring is enabled for the EC2 instance."
-  value       = aws_instance.var.ec2_name.monitoring
+  value       = aws_instance.nginx_instance.monitoring
 }
 
 output "security_group_ids" {
   description = "The list of security group IDs assigned to the EC2 instance."
-  value       = aws_instance.var.ec2_name.vpc_security_group_ids
+  value       = aws_instance.nginx_instance.vpc_security_group_ids
 }
 
 output "key_name" {
   description = "The name of the key pair associated with the EC2 instance."
-  value       = aws_instance.var.ec2_name.key_name
+  value       = aws_instance.nginx_instance.key_name
 }
 
 output "subnet_id" {
   description = "The ID of the subnet to which the instance is attached."
-  value       = aws_instance.var.ec2_name.subnet_id
+  value       = aws_instance.nginx_instance.subnet_id
 }
 
 output "tags" {
   description = "A map of tags assigned to the resource."
-  value       = aws_instance.var.ec2_name.tags
+  value       = aws_instance.nginx_instance.tags
 }
