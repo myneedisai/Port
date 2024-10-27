c67de38f448027d76523455a3902523ccc7d9e62
myneedisai <myneedisai@gmail.com>
2024-10-26 21:01:01 -0500

    Create variables.tf

---
+++ EC2/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the resources in"
+  type        = string
+  default     = "us-west-1"  # Change as necessary
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
+  default     = "ami-0da424eb883458071"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  type        = string
+  default     = "t2.micro"  # Change as necessary
+}
+
+variable "key_name" {
+  description = "The key name to use for the EC2 instance"
+  type        = string
+}
