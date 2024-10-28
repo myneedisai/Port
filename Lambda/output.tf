d56734537a7fcd97924a131269fba617656c0582
myneedisai <myneedisai@gmail.com>
2024-10-28 12:43:09 -0500

    Update output.tf

--- Lambda/output.tf
+++ Lambda/output.tf
@@ -1,3 +1,19 @@
+output "terraform_aws_role_output" {
+  description = "The name of the IAM role created for the Lambda function."
+  value       = aws_iam_role.lambda_role.name
+}
+
+output "terraform_aws_role_arn_output" {
+  description = "The ARN of the IAM role created for the Lambda function."
+  value       = aws_iam_role.lambda_role.arn
+}
+
+output "terraform_logging_arn_output" {
+  description = "The ARN of the IAM policy for logging."
+  value       = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+# Additional Outputs
 output "lambda_function_name" {
   description = "The name of the created Lambda function."
   value       = aws_lambda_function.terraform_lambda_func.function_name
