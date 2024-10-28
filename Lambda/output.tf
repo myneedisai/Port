a8eeaa874afa4fde9d27e59b6865e149a6e315a7
myneedisai <myneedisai@gmail.com>
2024-10-28 12:39:07 -0500

    Update output.tf

--- Lambda/output.tf
+++ Lambda/output.tf
@@ -1,15 +1,9 @@
-output "lambda_arn" {
-  value = aws_lambda_function.my_lambda.arn
+output "lambda_function_name" {
+  description = "The name of the created Lambda function."
+  value       = aws_lambda_function.terraform_lambda_func.function_name
 }
 
-output "lambda_name" {
-  value = aws_lambda_function.my_lambda.function_name
-}
-
-output "lambda_runtime" {
-  value = aws_lambda_function.my_lambda.runtime
-}
-
-output "lambda_handler" {
-  value = aws_lambda_function.my_lambda.handler
+output "lambda_function_arn" {
+  description = "The ARN of the created Lambda function."
+  value       = aws_lambda_function.terraform_lambda_func.arn
 }
